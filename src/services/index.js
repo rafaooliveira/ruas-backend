5f4e0b55a9774bde5a6d05e8c3cf738c68573df6
rafaooliveira <raafasantos1@gmail.com>
2021-09-22 21:47:55 -0300

    users with properties of artists and promotores

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,5 @@
 const users = require('./users/users.service.js');
-const promotor = require('./promotor/promotor.service.js');
-const artista = require('./artista/artista.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
-  app.configure(promotor);
-  app.configure(artista);
 };
